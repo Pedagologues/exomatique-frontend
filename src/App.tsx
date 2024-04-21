aee0b8fbac11584cbce92be2e167b4a5b1ec6058
Wyrdix <wyrdix@gmail.com>
2024-04-21 13:06:34 +0200

    Add simple header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./features/credentials/Login";
 import Logout from "./features/credentials/Logout";
 import Register from "./features/credentials/Register";
+import Home from "./features/home/Home";
+import Header from "./features/home/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <BrowserRouter>
         <Routes>
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
           <Route path="logout" element={<Logout />} />
+          <Route path="home" element={<Home />} />
+          <Route path="/" element={<Navigate to={"home"} replace />} />
         </Routes>
       </BrowserRouter>
     </div>
