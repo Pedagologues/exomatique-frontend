4ac21b59ceb8c926937a00e5f007844d19b19220
Wyrdix <wyrdix@gmail.com>
2024-05-11 18:24:56 +0200

    Add a home directory

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,3 +1,49 @@
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { IconButton, Paper, Typography } from "@mui/material";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <Paper
+      style={{
+        margin: 20,
+        height: "100%",
+        position: "relative",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flex: 1,
+          justifyContent: "center",
+          flexDirection: "column"
+        }}
+      >
+        <Typography variant="h1" color="error" style={{
+          textAlign: "center"
+        }}>
+          Ce site est en pré-alpha
+        </Typography>
+        
+        <Typography variant="h5" margin={5}>
+          Si vous voulez contribuer au code, donner une idée ou bien faire connaître un bug n'hésitez pas à le mettre sur le GitHub. (lien en bas à droite)
+
+          Ce projet est pas essence communautaire, si vous avez un avis (constructif) on serait ravis de l'entendre !
+        </Typography>
+      </div>
+
+      <IconButton
+        style={{
+          position: "absolute",
+          right: 0,
+          bottom: 0,
+        }}
+        size="large"
+        href="https://github.com/Pedagologues/exomatique"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <GitHubIcon style={{ fontSize: 60 }} />
+      </IconButton>
+    </Paper>
+  );
 }
